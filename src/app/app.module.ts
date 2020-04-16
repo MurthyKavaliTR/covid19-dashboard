b5932ece838312db83073e09710f3283c2a83576
Murthy Kavali <VenkataSatyaTrimurthy.Kavali@thomsonreuters.com>
2020-04-16 12:23:33 +0530

    Added materailUI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UpdatesComponent } from './updates/updates.component';
+import { FactsComponent } from './facts/facts.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UpdatesComponent,
+    FactsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
