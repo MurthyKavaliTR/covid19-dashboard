ae3ceea257e8fa22bb5270860d144b111b6232bc
Ramchandra Thopugonda <Ramchandra.Thopugonda@thomsonreuters.com>
2020-04-19 16:03:22 +0530

    changes made in fact component

--- src/app/facts/facts.component.ts
+++ src/app/facts/facts.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+ 
 
 @Component({
   selector: 'app-facts',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./facts.component.css']
 })
 export class FactsComponent implements OnInit {
+  factoids:Array<any>;
+  faq:Array<any>
+  bannerText:string;
 
-  constructor() { }
+
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<any>('https://api.covid19india.org/website_data.json').
+    subscribe(data=>{
+      console.log(data)
+      this.factoids = data.factoids;
+      this.faq = data.faq;
+      this.bannerText = this.factoids[1].banner;
+    })
   }
+  
 
 }
